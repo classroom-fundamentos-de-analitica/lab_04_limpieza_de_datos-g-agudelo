f60193391f79ccf0d6a6da69138962d68c25707b
g-agudelo <126741130+g-agudelo@users.noreply.github.com>
2024-03-31 18:42:39 -0500

    limpieza de datos Lab_04

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,20 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df.copy()
+    df["sexo"] = df["sexo"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df["idea_negocio"] = df["idea_negocio"].str.lower()
+    df["barrio"] = df["barrio"].str.lower()
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"],format="mixed")
+    df["fecha_de_beneficio"] = df["fecha_de_beneficio"].dt.strftime('%m/%d/%Y')
+    df["monto_del_credito"] = df["monto_del_credito"].str.replace("$","").str.replace(".00","").str.replace(",","").astype(int)
+    df["línea_credito"] = df["línea_credito"].str.lower()
+    df.drop_duplicates(inplace=True)
+    df.dropna(subset=["tipo_de_emprendimiento","barrio"],inplace=True)
     return df
+
